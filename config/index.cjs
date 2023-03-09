b65828ca8cdcf6d5631db41f62abb4edf7a92354
zangxiongao666 <110096912+zangxiongao666@users.noreply.github.com>
2023-03-09 11:15:46 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'onRDb6akzKW6ukfmRqljhP_Khkik',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	XkZyr1xr5AppfWB868Sv-l9JZpjiwlNdIt4w308KCws',
+      useTemplateId: 'XkZyr1xr5AppfWB868Sv-l9JZpjiwlNdIt4w308KCws',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-14',
       festivals: [
